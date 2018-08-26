4e8d577be840f744f88d3d3185244658df1dd07e
Natsu Dragneel <lagaleno@natsu.local>
2018-08-25 22:36:41 -0300

    + pagina de notas para os pais acompanharem

--- laravel/resources/views/notas.blade.php
+++ laravel/resources/views/notas.blade.php
@@ -8,7 +8,6 @@
 	  		<a href="#" data-target="mobile-demo" class="right sidenav-trigger"><i class="material-icons">menu</i></a>
   			<ul class="right hide-on-med-and-down">
     			<li><a lass="active" href="/perfil">Perfil</a></li>
-    			<li><a href="/matricula">Situação Matricula</a></li>
     			<li><a href="/acompanhamento">Acompanhamento do Municipio</a></li>
     			<li><a href="collapsible.html">Sair da conta</a></li>
   			</ul>
@@ -17,7 +16,6 @@
 
 	<ul class="sidenav" id="mobile-demo">
   	<li><a href="/perfil">Perfil</a></li>
-  	<li><a href="/matricula">Situação da Matricula</a></li>
   	<li><a href="/acompanhamento">Acompanhamento do Municipio</a></li>
   	<li><a href="collapsible.html">Sair da Conta</a></li>
   </ul>
@@ -26,6 +24,76 @@
     <div class='row'>
         <a href='/filho' class='waves-effect waves-light btn-back margin'><i class="material-icons left">arrow_back</i>Voltar para a página anterior</a>
     </div>
+
+    <table>
+        <thead>
+          <tr>
+              <th></th>
+              <th>Prova 1</th>
+              <th>Prova 2</th>
+              <th>Prova 3</th>
+              <th>Prova 4</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Matemática</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr>
+            <td>Português</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr>
+            <td>Inglês</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr> 
+            <td> Ciência </td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr>
+            <td> História </td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr> 
+            <td> Geografia </td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr> 
+            <td> Educação Física </td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr>
+            <td> Artes </td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+        </tbody>
+      </table>
   </div>
 
   @include('footer')
